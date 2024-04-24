a2839941bc8a0cb22da8990422e87e06e3caa934
Rokas <rookas.rudzenskas@gmail.com>
2024-04-24 12:43:25 +0300

    api outes

--- app/api/followers/route.ts
+++ app/api/followers/route.ts
@@ -1,88 +1,100 @@
-import mongoose, { Schema, Document, Model, models } from "mongoose";
+import connectDB from "@/mongodb/db";
+import { Followers } from "@/mongodb/models/followers";
+import { NextResponse } from "next/server";
 
-export interface IFollowersBase {
-  follower: string;
-  following: string;
-}
+// GET function is used to get all followers of a user
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const user_id = searchParams.get("user_id");
 
-// Define the document methods (for each instance of a follower relationship)
-interface IFollowersMethods {
-  unfollow(): Promise<void>;
-}
-
-// Define the document interface
-export interface IFollowers
-  extends IFollowersBase,
-    Document,
-    IFollowersMethods {
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-// Define the static methods
-interface IFollowersStatics {
-  follow(follower: string, following: string): Promise<IFollowers>;
-  getAllFollowers(userId: string): Promise<IFollowers[]>;
-  getAllFollowing(userId: string): Promise<IFollowers[]>;
-}
-
-// Merge the document methods, and static methods with IFollowers
-interface IFollowersModel extends Model<IFollowers>, IFollowersStatics {}
-
-const FollowersSchema = new Schema<IFollowers>(
-  {
-    follower: { type: String, required: true },
-    following: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-FollowersSchema.methods.unfollow = async function () {
   try {
-    await this.deleteOne({ _id: this._id });
-  } catch (error) {
-    console.log("error when unfollowing", error);
-  }
-};
+    await connectDB();
 
-FollowersSchema.statics.follow = async function (
-  follower: string,
-  following: string
-) {
-  try {
-    const existingFollow = await this.findOne({ follower, following });
-
-    if (existingFollow) {
-      throw new Error("You are already following this user");
+    if (!user_id) {
+      return NextResponse.json(
+        { error: "User ID not provided" },
+        { status: 400 }
+      );
     }
 
-    const follow = await this.create({ follower, following });
-    return follow;
-  } catch (error) {
-    console.log("error when following", error);
-  }
-};
+    const followers = await Followers.getAllFollowers(user_id);
 
-FollowersSchema.statics.getAllFollowers = async function (userId: string) {
+    if (!followers) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(followers);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while fetching followers" },
+      { status: 500 }
+    );
+  }
+}
+
+export interface FollowerRequestBody {
+  followerUserId: string;
+  followingUserId: string;
+}
+
+// POST function is used to add a follower to a user
+export async function POST(request: Request) {
+  const { followerUserId, followingUserId }: FollowerRequestBody =
+    await request.json();
   try {
-    const followers = await this.find({ following: userId });
-    return followers;
-  } catch (error) {
-    console.log("error when getting all followers", error);
-  }
-};
+    await connectDB();
 
-FollowersSchema.statics.getAllFollowing = async function (userId: string) {
+    const follow = await Followers.follow(followerUserId, followingUserId);
+
+    if (!follow) {
+      return NextResponse.json(
+        { error: "Follow action failed" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ message: "Followed successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while following" },
+      { status: 500 }
+    );
+  }
+}
+
+// DELETE function is used to remove a follower from a user
+export async function DELETE(request: Request) {
+  const { followerUserId, followingUserId }: FollowerRequestBody =
+    await request.json();
+
   try {
-    const following = await this.find({ follower: userId });
-    return following;
-  } catch (error) {
-    console.log("error when getting all following", error);
-  }
-};
+    await connectDB();
 
-export const Followers =
-  (models.Followers as IFollowersModel) ||
-  mongoose.model<IFollowers, IFollowersModel>("Followers", FollowersSchema);
+    if (!followerUserId || !followingUserId) {
+      return NextResponse.json(
+        { error: "Follower ID or Following ID not provided" },
+        { status: 400 }
+      );
+    }
+
+    const follow = await Followers.findOne({
+      follower: followerUserId,
+      following: followingUserId,
+    });
+
+    if (!follow) {
+      return NextResponse.json(
+        { error: "Follow relationship not found" },
+        { status: 404 }
+      );
+    }
+
+    await follow.unfollow();
+    return NextResponse.json({ message: "Unfollowed successfully" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while unfollowing" },
+      { status: 500 }
+    );
+  }
+}
