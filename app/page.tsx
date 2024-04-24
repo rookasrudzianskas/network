2660b35da92b58f13671325b0c18f9f64a2a46a5
Rokas <rookas.rudzenskas@gmail.com>
2024-04-24 12:30:36 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,34 @@
-import Image from "next/image";
-import UserInformation from "@/components/user-information";
 import PostForm from "@/components/post-form";
-import {SignedIn} from "@clerk/nextjs";
-import connectDB from "@/mongodb/db";
-import Post from "@/components/post";
 import PostFeed from "@/components/post-feed";
+import UserInformation from "@/components/user-information";
+import Widget from "@/components/widget";
+import { Post } from "@/mongodb/models/post";
+import { SignedIn } from "@clerk/nextjs";
+import connectDB from "@/mongodb/db";
+import React from "react";
+
+export const revalidate = 0;
 
 export default async function Home() {
   await connectDB();
   const posts = await Post.getAllPosts();
 
   return (
-    <main className={"grid grid-cols-8 mt-5 sm:px-5"}>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
-        <UserInformation posts={posts}/>
+        <UserInformation posts={posts} />
       </section>
 
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
         <SignedIn>
-          <PostForm/>
+          <PostForm />
         </SignedIn>
-        <PostFeed posts={posts}/>
+        <PostFeed posts={posts} />
       </section>
 
       <section className="hidden xl:inline justify-center col-span-2">
-      {/*  <Widget/>*/}
+        <Widget />
       </section>
-    </main>
+    </div>
   );
 }
