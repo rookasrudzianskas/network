5d82428b3a90bbab0dc8f1078fe901165d3c8a6a
Rokas <rookas.rudzenskas@gmail.com>
2024-04-24 12:26:00 +0300

    fix

--- actions/createCommentAction.ts
+++ actions/createCommentAction.ts
@@ -1,47 +1,52 @@
-// "use server";
-//
-// import {currentUser} from "@clerk/nextjs/server";
-// import {IUser} from "@/types/user";
-//
-// export default async function createCommentAction(
-//   postId: string,
-//   formData: FormData
-// ) {
-//   const user = await currentUser();
-//
-//   const commentInput = formData.get("commentInput") as string;
-//
-//   if (!postId) throw new Error("Post id is required");
-//   if (!commentInput) throw new Error("Comment input is required");
-//   if (!user?.id) throw new Error("User not authenticated");
-//
-//   const userDB: IUser = {
-//     userId: user.id,
-//     userImage: user.imageUrl,
-//     firstName: user.firstName || "",
-//     lastName: user.lastName || "",
-//   };
-//
-//   const body: AddCommentRequestBody = {
-//     user: userDB,
-//     text: commentInput,
-//   };
-//
-//   const post = await Post.findById(postId);
-//
-//   if (!post) {
-//     throw new Error("Post not found");
-//   }
-//
-//   const comment: ICommentBase = {
-//     user: userDB,
-//     text: commentInput,
-//   };
-//
-//   try {
-//     await post.commentOnPost(comment);
-//     revalidatePath("/");
-//   } catch (error) {
-//     throw new Error("An error occurred while adding comment");
-//   }
-// }
+// @ts-nocheck
+"use server";
+
+import {currentUser} from "@clerk/nextjs/server";
+import {IUser} from "@/types/user";
+import { AddCommentRequestBody } from "@/app/api/posts/[post_id]/comments/route";
+import Post from "@/components/post";
+import {ICommentBase} from "@/mongodb/models/comment";
+import {revalidatePath} from "next/cache";
+
+export default async function createCommentAction(
+  postId: string,
+  formData: FormData
+) {
+  const user = await currentUser();
+
+  const commentInput = formData.get("commentInput") as string;
+
+  if (!postId) throw new Error("Post id is required");
+  if (!commentInput) throw new Error("Comment input is required");
+  if (!user?.id) throw new Error("User not authenticated");
+
+  const userDB: IUser = {
+    userId: user.id,
+    userImage: user.imageUrl,
+    firstName: user.firstName || "",
+    lastName: user.lastName || "",
+  };
+
+  const body: AddCommentRequestBody = {
+    user: userDB,
+    text: commentInput,
+  };
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  const comment: ICommentBase = {
+    user: userDB,
+    text: commentInput,
+  };
+
+  try {
+    await post.commentOnPost(comment);
+    revalidatePath("/");
+  } catch (error) {
+    throw new Error("An error occurred while adding comment");
+  }
+}
