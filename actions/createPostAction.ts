655e040104188bbdad356ee8ca6002c81651f538
Rokas <rookas.rudzenskas@gmail.com>
2024-04-24 11:02:16 +0300

    Fix

--- actions/createPostAction.ts
+++ actions/createPostAction.ts
@@ -3,8 +3,8 @@
 // import { AddPostRequestBody } from "@/app/api/posts/route";
 import generateSASToken, { containerName } from "@/lib/generateSASToken";
 
-// import { Post } from "@/mongodb/models/post";
-// import { IUser } from "@/types/user";
+import { Post } from "@/mongodb/models/post";
+import { IUser } from "@/types/user";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { currentUser } from "@clerk/nextjs/server";
 import { randomUUID } from "crypto";
@@ -30,5 +30,53 @@ export default async function createPostAction(formData: FormData) {
     firstName: user.firstName || "",
     lastName: user.lastName || "",
   };
+
+  try {
+    if (image.size > 0) {
+      console.log("Uploading image to Azure Blob Storage...", image);
+
+      const accountName = process.env.AZURE_STORAGE_NAME;
+
+      const sasToken = await generateSASToken();
+
+      const blobServiceClient = new BlobServiceClient(
+        `https://${accountName}.blob.core.windows.net?${sasToken}`
+      );
+
+      const containerClient =
+        blobServiceClient.getContainerClient(containerName);
+
+      // generate current timestamp
+      const timestamp = new Date().getTime();
+      const file_name = `${randomUUID()}_${timestamp}.png`;
+
+      const blockBlobClient = containerClient.getBlockBlobClient(file_name);
+
+      const imageBuffer = await image.arrayBuffer();
+      const res = await blockBlobClient.uploadData(imageBuffer);
+      image_url = res._response.request.url;
+
+      console.log("File uploaded successfully!", image_url);
+
+      // const body: AddPostRequestBody = {
+      //   user: userDB,
+      //   text: postInput,
+      //   imageUrl: image_url,
+      // };
+
+      // await Post.create(body);
+    } else {
+      // const body: AddPostRequestBody = {
+      //   user: userDB,
+      //   text: postInput,
+      // };
+      //
+      // await Post.create(body);
+    }
+  } catch (error: any) {
+    throw new Error("Failed to create post", error);
+  }
+
+  revalidatePath("/");
 
 }
